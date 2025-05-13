d581eae346dc1b7a35c75f75351514cb91ca5c77
Antonio <antonio.ibarra.g46@gmail.com>
2025-05-13 10:06:01 -0600

    Removed some code and added singed validator

--- spending-app/pages/api/utils.ts
+++ spending-app/pages/api/utils.ts
@@ -1,29 +1,4 @@
-import { applyParamsToScript, BlockfrostProvider, Data, MeshTxBuilder, PlutusScript } from "@meshsdk/core";
-import plutusScript from "../../onchain/plutus.json";
-import cbor from "cbor";
-
-export function getPlutusScript(title: String): PlutusScript {
-  return {
-    code: cbor
-      .encode(Buffer.from(plutusScript.validators.filter((val: any) => val.title === title)[0].compiledCode, "hex"))
-      .toString("hex"),
-    version: "V3",
-  };
-}
-
-export function getPlutusScriptParams(title:string, params: object[] | Data[]): PlutusScript {
-  const scriptCbor = applyParamsToScript(
-    cbor.encode(Buffer.from(plutusScript.validators.filter((val: any) => val.title === title)[0].compiledCode, "hex"))
-      .toString("hex"),
-    params,
-    "Mesh",
-  );
-
-  return  {
-    code: scriptCbor,
-    version: "V3"
-  }
-}
+import { BlockfrostProvider, MeshTxBuilder } from "@meshsdk/core";
 
 export function initializeBlockchainProvider(): BlockfrostProvider {
   const apiKey = process.env.NEXT_PUBLIC_BLOCKFROST_API_KEY;
